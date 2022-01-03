fbf7cbf19daf6a46095106b1364679ad87b80095
Hyago M. Vale <hyagomvale@gmail.com>
2022-01-03 18:51:32 -0300

    @ComponentScan

--- src/main/java/com/github/hyagomv/spring/Application.java
+++ src/main/java/com/github/hyagomv/spring/Application.java
@@ -3,11 +3,13 @@ package com.github.hyagomv.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.github.hyagomv.bean.ClassA;
 import com.github.hyagomv.spring.bean.ClassB;
 
-@SpringBootApplication(scanBasePackages = "com.github.hyagomv")
+@SpringBootApplication
+@ComponentScan("com.github.hyagomv")
 public class Application {
 
 	public static void main(String[] args) {
