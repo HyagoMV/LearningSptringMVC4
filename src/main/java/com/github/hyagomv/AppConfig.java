a489b9b9bd2908989eff5d569f6a639785039275
Hyago M. Vale <hyagomvale@gmail.com>
2022-01-03 14:24:51 -0300

    Init

---
+++ src/main/java/com/github/hyagomv/AppConfig.java
@@ -0,0 +1,13 @@
+package com.github.hyagomv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class AppConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConfig.class);
+	}
+	
+}
